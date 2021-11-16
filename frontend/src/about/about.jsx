4e9b959ac9d520338d3676701ebb031dcd4d007a
FranciscoCFranco <franciscofrancodev@gmail.com>
2021-11-16 18:29:46 -0300

    Lista de taregas concluído

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Pageheader from "../template/pageheader";
+
+export default props => (
+  <div>
+    <Pageheader name='Sobre' small='Nós'></Pageheader>
+
+    <h2>Nossa Histório</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Missão e visão</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+  </div>
+)
